feb3f396501267a35b9d0179f8eb60ee39e14ebd
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2023-08-19 14:52:37 +0800

    Refactor manhattanDistance method and its helper functions

--- js/astar.js
+++ js/astar.js
@@ -21,27 +21,6 @@ function aStarSearch(start, goal=[0,1,2,3,4,5,6,7,8]) {
     }
     let solution = reconstructPath(start, visited);
     let endTime = performance.now()
-    console.log(`Found solution in ${(endTime-startTime)/1000} seconds. ${statesExpanded} states expanded`)
+    console.log(`Start: ${start}. Found solution in ${(endTime-startTime)/1000} seconds. ${statesExpanded} states expanded`)
     return solution;
-}
-
-
-// turn a 1d array of length 9 into a 2d array (3x3 grid)
-// used for calculating manhattan distance
-function transform2D(a) {
-    let arr = a.slice();
-    const transformed = [];
-    while (arr.length) {
-        transformed.push(arr.splice(0,3));
-    }
-    return transformed
-}
-
-
-// get [row, col] of an item in a 2d array
-function getXY(board, item) {
-    for (let i = 0; i < 3; i++)
-        for (let j = 0; j < 3; j++)
-            if (board[i][j] == item)
-                return [i, j]; 
 }
--- js/node.js
+++ js/node.js
@@ -42,18 +42,19 @@ class Node {
     }
 
     get manhattanDistance() {
-        let goal=[0,1,2,3,4,5,6,7,8];
-        let state2D = transform2D(this.state);
-        let goal2D = transform2D(goal);
+        let goal = [0,1,2,3,4,5,6,7,8];
         let distance = 0;
         let xS, yS, xG, yG;
+
         for (let i = 0; i < goal.length; i++) {
-            if (this.state[i] != 0) {
-                [xS, yS] = getXY(state2D, this.state[i]);   
-                [xG, yG] = getXY(goal2D, this.state[i]);
-                distance += Math.abs(xS - xG) + Math.abs(yS - yG);
-            }
+            [xS, yS] = getXY(this.state, this.state[i]);
+            [xG, yG] = getXY(goal, this.state[i]);
+            distance += Math.abs(xS - xG) + Math.abs(yS - yG);
         }
+
         return distance
     }
 }
+
+// get [row, col of an item in array of length 9, as if it was a 3x3 array
+const getXY = (board, item) => [Math.floor(board.indexOf(item)/3), board.indexOf(item)%3]
